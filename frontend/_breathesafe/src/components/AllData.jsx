9e9a2ab71a200d1c25a33c7049111624a628c79f
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-01 13:45:14 +0800

    test 8.1

--- frontend/_breathesafe/src/components/AllData.jsx
+++ frontend/_breathesafe/src/components/AllData.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import formatTimestamp from './functions/formatTimestamp'
 import SensorChart from './SensorChart'
 import Table from './Table'
 
