e48fff0ab015e4df130936bc1432ce469619063d
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-01 14:50:18 +0800

    test 1

--- frontend/_breathesafe/src/components/NavBar.jsx
+++ frontend/_breathesafe/src/components/NavBar.jsx
@@ -27,7 +27,7 @@ function NavBar() {
   return (
     <div className="flex navbar sticky top-0 z-10 mt-1.5 mx-1.5 h-24 w-[99%] bg-darkblue text-white rounded-2xl shadow-black/50 shadow-md">
       <div className="navbar-start mt-3">
-        <img src='logo192.png' className='bg-black size-<50%>'/>
+        <img src='longlogo.png' className='bg-black size-<50%>'/>
         <div className='flex flex-col pl-5'>
           <a className="font-bold text-3xl ml-2">Air Quality Monitor</a>
           <Clock />
