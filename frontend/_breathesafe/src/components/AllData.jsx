486d0b27e1eae836920101ba81ea917f1a768ef8
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-01 12:43:50 +0800

    test 6

--- frontend/_breathesafe/src/components/AllData.jsx
+++ frontend/_breathesafe/src/components/AllData.jsx
@@ -15,6 +15,7 @@ function AllData() {
       try {
         const response = await axios.get(`https://breath-o9r9.onrender.com/api/sensor_data/all`)
         const sensorData = response.data
+        console.log("Sensor data: " + sensorData)
         
         const pm25ChartData = sensorData.reduce((acc, { id, emp_name, pm25, timestamp }) => {
           if (!acc[emp_name]) {
