e4b44c2bb2b467683bc16fb7304381b39337a700
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-01 11:25:03 +0800

    test all data layout

--- frontend/_breathesafe/src/components/AllData.jsx
+++ frontend/_breathesafe/src/components/AllData.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function AllData() {
   return (
-    <div>AllData</div>
+    <div className='absolute inset-0 bg-background min-h-screen overflow-auto pt-5'>
+      <div className='grid grid-cols-2 gap-4 pr-4 max-lg:grid-cols-1'>
+        <div className='grid grid-rows-4 gap-4 pr-4 max-lg:grid-cols-1'>
+          <div className='bg-red-500'></div>
+          <div className='bg-green-500'></div>
+          <div className='bg-blue-500'></div>
+          <div className='bg-yellow-500'></div>
+        </div>
+        <div className='grid grid-rows-4 gap-4 pr-4 max-lg:grid-cols-1'>
+          <div className='bg-pink-500'></div>
+          <div className='bg-orange-500'></div>
+          <div className='bg-emerald-950'></div>
+          <div className='bg-cyan-500'></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/_breathesafe/src/components/Records.jsx
+++ frontend/_breathesafe/src/components/Records.jsx
@@ -3,9 +3,9 @@ import EmpTable from './EmpTable'
 
 function Recods() {
   return (
-    <main className='absolute inset-0 bg-background min-h-screen overflow-auto pt-5'>
+    <div className='absolute inset-0 bg-background min-h-screen overflow-auto pt-5'>
       <EmpTable />
-    </main>
+    </div>
   )
 }
 
