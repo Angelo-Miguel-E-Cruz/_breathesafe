3bfb2012b7f34a0df8d24c5fe6100d15f2a2f882
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-01 14:32:52 +0800

    navbar fix

--- frontend/_breathesafe/src/components/NavBar.jsx
+++ frontend/_breathesafe/src/components/NavBar.jsx
@@ -27,8 +27,8 @@ function NavBar() {
   return (
     <div className="flex navbar sticky top-0 z-10 mt-1.5 mx-1.5 h-24 w-[99%] bg-darkblue text-white rounded-2xl shadow-black/50 shadow-md">
       <div className="navbar-start mt-3">
-        <div className='grid grid-cols-2 bg-black'>
-          <img src='longlogo.png'/>
+        <div className='grid grid-cols-2'>
+          <img src='longlogo.png' className='bg-black'/>
           <div className='flex flex-col pl-5'>
             <a className="font-bold text-3xl ml-2">Air Quality Monitor</a>
             <Clock />
