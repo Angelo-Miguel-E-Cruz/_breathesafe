b464e7518857f32bd859a2335aefb4b20d7f1c6d
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-01 12:06:02 +0800

    test 3

--- frontend/_breathesafe/src/components/AllData.jsx
+++ frontend/_breathesafe/src/components/AllData.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import SensorChart from './SensorChart'
 import Table from './Table'
