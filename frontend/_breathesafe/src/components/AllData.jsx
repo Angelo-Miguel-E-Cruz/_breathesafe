f03cf3d790d037a82b0649dbd3493d8d61276c1c
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-01 11:40:00 +0800

    test getting all data

--- frontend/_breathesafe/src/components/AllData.jsx
+++ frontend/_breathesafe/src/components/AllData.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import SensorChart from './SensorChart'
+import Table from './Table'
 
 function AllData() {
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://breath-o9r9.onrender.com/api/sensor_data/all`)
+        const sensorData = response.data
+
+        console.log(sensorData)
+        
+      } catch (error) {
+        console.log(error.message) 
+      }
+    }
+
+    fetchData
+    const interval = setInterval(fetchData, 5000)
+
+    return () => clearInterval(interval)
+
+  })
+
   return (
-    <div className='absolute inset-0 bg-background min-h-screen overflow-auto pt-5'>
+    <div className='absolute inset-0 bg-background h-screen pt-27 overflow-x-auto w-full'>
       <div className='grid grid-cols-2 gap-4 pr-4 max-lg:grid-cols-1'>
         <div className='grid grid-rows-4 gap-4 pr-4 max-lg:grid-cols-1'>
           <div className='bg-red-500'>1</div>
