3b3cb44c7215b09529a7585d306f8c58f51d3b2d
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-02 19:42:52 +0800

    test 1.12

--- frontend/_breathesafe/src/components/EmpTable.jsx
+++ frontend/_breathesafe/src/components/EmpTable.jsx
@@ -227,7 +227,7 @@ function EmpTable() {
         </div>
         <button className='btn btn-success' onClick={handleNav}>!</button>
         <button className='btn btn-warning' onClick={addEmployee}>!</button>
-        <label className="input input-bordered border-black text-black bg-background focus:shadow-black/50 focus:shadow-md
+        <label className="input input-bordered border-black text-black bg-background shadow-black/50 shadow-sm
                           flex items-center gap-2 absolute top-23 right-0">
           <input type="search" className="grow" placeholder="Search" onChange={handleSearchChange} />
           <MdSearch />
