2a8ee985a8527d81f5d91a81c80f67bddef6976e
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-05 22:08:29 +0800

    test compute 5 minute average

--- frontend/_breathesafe/src/components/functions/timer.js
+++ frontend/_breathesafe/src/components/functions/timer.js
@@ -26,5 +26,19 @@ export const startLoopingCountdown = (durationInSeconds, callback) => {
 }
 
 export const onTimerEnd = async() => {
-  
+  const interval = durationInSeconds === 300 ? '5 minutes' : ''
+  try {
+    const response = await axios.get(`https://breath-o9r9.onrender.com/api/interval_data`, { interval })
+    const pm25Array = data.map(item => parseFloat(item.pm25))
+    const pm10Array = data.map(item => parseFloat(item.pm10))
+    const aqiPm25Array = data.map(item => item.aqi_pm25)
+    const aqiPm10Array = data.map(item => item.aqi_pm10)
+
+    console.log("pm25Array:", pm25Array)
+    console.log("pm10Array:", pm10Array)
+    console.log("aqiPm25Array:", aqiPm25Array)
+    console.log("aqiPm10Array:", aqiPm10Array)
+  } catch (error) {
+    console.log(error.message)
+  }
 }
