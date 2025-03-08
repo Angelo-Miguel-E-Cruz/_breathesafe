09695851fda93b2d83b56e11a5a053edf15a3953
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-08 09:44:52 +0800

    debugging

--- backend/src/controllers/sensorControllers.js
+++ backend/src/controllers/sensorControllers.js
@@ -26,10 +26,9 @@ export const getAllSensorData = async (req, res) => {
 
 export const getDatainRange = async (req, res) => {
   try{
-    const { interval } = req.body
-    console.log("interval: ", interval)
+    const { interval } = req.bodys
     const items = await sensorService.getDatainRange(interval)
-    res.status(200).json(items)
+    res.status(200).json({result: items, interval: interval, type: typeof(interval)})
   } catch (err){
     console.error(err)
     res.status(500).json({message: "Server Error"})
