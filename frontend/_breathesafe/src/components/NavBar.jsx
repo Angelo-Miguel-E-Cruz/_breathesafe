be9de7c439dccc863fbc17aa1b132ee589ed4ff7
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-08 23:41:43 +0800

    t2

--- frontend/_breathesafe/src/components/NavBar.jsx
+++ frontend/_breathesafe/src/components/NavBar.jsx
@@ -57,23 +57,27 @@ function NavBar({role}) {
             isActive || location.pathname.startsWith('/admin') || location.pathname.startsWith('/dashboard') ?  
             'bg-blue_green' : 'bg-darkblue hover:bg-white/20'}`} onClick={() => setSettingsOpen(false)}> <RiHome9Fill /> Home</NavLink>
           {role === "Admin" ? 
-          <div className="relative block text-left p-2 w-28">
-            <button className='btn rounded-xl mr-2 shadow-black/50 shadow-md bg-darkblue hover:bg-white/20
-              transition duration-300 ease-in-out' onClick={() => setSettingsOpen(!settingsOpen)}> <IoSettingsSharp /> Settings</button>
-            {settingsOpen ?
-            <ul className='absolute left-0 top-full'>
-              <li> 
-                  <NavLink to='records' className={({isActive}) => 
-                    `w-28 flex items-center gap-2 px-4 py-2 z-10 ${isActive ? 'bg-blue_green' : 'bg-darkblue hover:bg-white/20'}`} onClick={() => setSettingsOpen(false)}> <IoNewspaper /> Records
-                  </NavLink>
-              </li>
-              <li> 
-                  <NavLink to='register' className={({isActive}) => 
-                    `w-28 flex items-center gap-2 px-4 py-2 z-10 ${isActive ? 'bg-blue_green' : 'bg-darkblue hover:bg-white/20'}`} onClick={() => setSettingsOpen(false)}> <MdPersonAdd /> Add Employee
-                  </NavLink>
-              </li>
-            </ul> : <></>}
-          </div> :  <></>}
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li><a>Settings</a></li>
+                <li><a>Logout</a></li>
+              </ul>
+            </div>:  <></>}
         </div>
       </div>
 
