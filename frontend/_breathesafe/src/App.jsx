8ddc29cc78d248c1bcf20759a19a301d561b1ffb
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-08 21:04:22 +0800

    h1

--- frontend/_breathesafe/src/App.jsx
+++ frontend/_breathesafe/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from './components/Layout'
 import Main from './components/Main'
 import Records from './components/Records'
