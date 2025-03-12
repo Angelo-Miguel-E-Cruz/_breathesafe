eb88f7d1b26424a3f328111dfd7e6f9c0598c10a
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-12 22:43:33 +0800

    t2

--- backend/src/controllers/regController.js
+++ backend/src/controllers/regController.js
@@ -6,7 +6,7 @@ import bcrypt from 'bcryptjs'
 export const regUser = async (req, res) => {
   try {
     console.log(req.body)
-    /*const { emp_name, password, role } = req.body
+    const { emp_name, emp_password, emp_role } = req.body
 
     // Check if user exists
     const doesExist = await regServices.checkExists(emp_name)
@@ -19,16 +19,23 @@ export const regUser = async (req, res) => {
     // Encrypt password
     const saltRound = 10
     const salt = await bcrypt.genSalt(saltRound)
-    const bcryptPassword = await bcrypt.hash(password, salt)
+    const bcryptPassword = await bcrypt.hash(emp_password, salt)
 
     // Register new user
-    const register = await regServices.registerUser(emp_name, bcryptPassword, role)
+    const register = await regServices.registerUser(emp_name, bcryptPassword, emp_role)
+
+    if (emp_role === "User"){
+      const { emp_id, device_id, emp_gender, emp_age } = req.body
+      const result = await regServices.addEmployee(emp_name, emp_id, device_id, emp_gender, emp_age)
+      if (!result){
+        return res.status(500).json({message: "Server Error", error: error.message})
+      }
+    }
 
     // Create user token
-    const token = jwtGenerator(register[0].user_id)*/
+    const token = jwtGenerator(register[0].user_id)
 
-    //return res.status(201).json({message: "User Added", token: token})
-    return res.status(201).json({message: "User Added"})
+    return res.status(201).json({message: "User Added", token: token})
   } catch (error) {
     console.log(error.message)
     return res.status(500).json({message: "Server Error", error: error.message})
--- backend/src/middleware/validInfo.js
+++ backend/src/middleware/validInfo.js
@@ -7,6 +7,12 @@ const valid = (req, res, next) =>{
     if(![emp_name, emp_password, emp_role].every(Boolean)){
       return res.status(401).json("Missing Credentials")
     } 
+    if (emp_role === "User"){
+      const { emp_id, device_id, emp_gender, emp_age } = req.body
+      if(![ emp_id, device_id, emp_gender, emp_age].every(Boolean)){
+        return res.status(401).json("Missing Employee Credentials")
+      } 
+    }
   } else if (req.path === "/login"){
     if(![password].every(Boolean)){
       return res.status(401).json("Missing Credentials")
