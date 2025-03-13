88df2131f4372de9cd8eb6043dacbe67f026e703
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-13 23:03:21 +0800

    t1

--- frontend/_breathesafe/src/components/UserData.jsx
+++ frontend/_breathesafe/src/components/UserData.jsx
@@ -15,8 +15,9 @@ function UserData() {
     try {
       const response = await axios.get('https://breath-o9r9.onrender.com/api/users')
 
-      const chartData = response.data.result.reduce((acc, { user_name, user_role}) => {
+      const chartData = response.data.result.reduce((acc, { user_id, user_name, user_role}) => {
         acc.push({
+          id: user_id,
           name: user_name, 
           role: user_role
         })
