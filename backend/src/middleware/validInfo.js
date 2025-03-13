d2f92098ae7f8e9a033db4b0b4fc079f2f9380fa
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-13 20:53:38 +0800

    t1.1

--- backend/src/middleware/validInfo.js
+++ backend/src/middleware/validInfo.js
@@ -1,18 +1,11 @@
 const valid = (req, res, next) =>{
 
   const { emp_name, emp_password, emp_role } = req.body
-  console.log(req.body)
 
   if(req.path === "/register"){
     if(![emp_name, emp_password, emp_role].every(Boolean)){
       return res.status(401).json("Missing Credentials")
     } 
-    if (emp_role === "User"){
-      const { emp_id, device_id, emp_gender, emp_age } = req.body
-      if(![ emp_id, device_id, emp_gender, emp_age].every(Boolean)){
-        return res.status(401).json("Missing Employee Credentials")
-      } 
-    }
   } else if (req.path === "/login"){
     if(![password].every(Boolean)){
       return res.status(401).json("Missing Credentials")
