173c1674d6b6b51865ee40ca8dcf211c90187e80
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-15 21:46:14 +0800

    lc14

--- frontend/_breathesafe/src/components/NameCard.jsx
+++ frontend/_breathesafe/src/components/NameCard.jsx
@@ -8,12 +8,12 @@ function NameCard({ employeeName }) {
     <>
     {
       role === "Admin" ? 
-      <div className="mb-2 card card-border border-lightblack w-fit bg-skyblue shadow-black/50 shadow-md">
+      <div className="card card-border border-lightblack w-fit bg-skyblue shadow-black/50 shadow-md">
         <div className="card-body p-4">
           <h1 className='text-lightgrey text-3xl font-bold'> Current Employee: {employeeName} </h1>
         </div>
       </div> :
-      <div className="mb-2 w-fit flex items-center space-x-2">
+      <div className="w-fit flex items-center space-x-2">
         <FaUserCircle className="text-lightgrey text-3xl" />
         <h1 className="text-lightgrey text-3xl font-bold">{employeeName}</h1>
       </div>
