220f4fa0a26f4e6f21a96ded9bec41b8d1da0393
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-15 22:04:32 +0800

    lc16

--- frontend/_breathesafe/src/components/NameCard.jsx
+++ frontend/_breathesafe/src/components/NameCard.jsx
@@ -3,23 +3,11 @@ import { FaUserCircle } from "react-icons/fa"
 
 function NameCard({ employeeName }) {
   
-  const role  = window.localStorage.getItem("role")
   return (
-    <>
-    {
-      role === "Admin" ? 
-      <div className="card card-border border-lightblack w-fit bg-skyblue shadow-black/50 shadow-md">
-        <div className="card-body p-4">
-          <h1 className='text-lightgrey text-3xl font-bold'> Current Employee: {employeeName} </h1>
-        </div>
-      </div> :
-      <div className="w-fit flex items-center space-x-3">
-        <FaUserCircle className="text-lightgrey text-3xl" />
-        <h1 className="text-lightgrey text-3xl font-bold">{employeeName}</h1>
-      </div>
-    
-    }
-    </>
+    <div className="w-fit flex items-center space-x-3">
+      <FaUserCircle className="text-lightgrey text-3xl" />
+      <h1 className="text-lightgrey text-3xl font-bold">{employeeName}</h1>
+    </div>
   )
 }
 
