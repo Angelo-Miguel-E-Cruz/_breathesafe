5df67a60e6f8246b3098d49d117bd05aeae7488d
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-17 22:37:43 +0800

    t2.8.1

--- frontend/_breathesafe/src/components/Login.jsx
+++ frontend/_breathesafe/src/components/Login.jsx
@@ -53,8 +53,8 @@ const Login = () => {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen ">
-      <div className="absolute inset-0 left-[50%] translate-x-[-50%] z-10 w-[850.51px] h-[1596.92px] origin-top-left rotate-[-9.11deg] bg-[radial-gradient(ellipse_158.87%_216.54%_at_148.77%_59.82%,_rgba(45,_45,_45,_0)_35%,_rgba(45,_45,_45,_0)_50%,_#2D2D2D_90%)]" />
+    <div className="flex justify-center items-center h-screen w-screen overflow-x-hidden">
+      <div className="absolute inset-0 left-[60%] translate-x-[-60%] z-10 w-[850.51px] h-[1596.92px] origin-top-left rotate-[-9.11deg] bg-[radial-gradient(ellipse_158.87%_216.54%_at_148.77%_59.82%,_rgba(45,_45,_45,_0)_35%,_rgba(45,_45,_45,_0)_50%,_#2D2D2D_90%)]" />
 
       <div className="w-full h-full flex overflow-hidden">
         {/* Left Section (Dark Overlay) */}
