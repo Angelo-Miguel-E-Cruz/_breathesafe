fa91d423e7965ab888119fc671ab78e48820a25e
AMEC17 <129676081+AMEC17@users.noreply.github.com>
2025-03-22 18:12:57 +0800

    t1.2

--- frontend/_breathesafe/src/components/Login.jsx
+++ frontend/_breathesafe/src/components/Login.jsx
@@ -65,46 +65,48 @@ const Login = () => {
           <img
             src="/loginlogo.png"
             alt="Breathesafe Logo"
-            className="absolute top-0 left-0"
+            className="absolute top-10 left-10 w-200 h-auto"
           />
-          <h1 className="text-3xl font-bold">Hello, welcome.</h1>
-          <p className="text-gray-400 mt-2">Please login your account.</p>
+          <div className='pl-20'>
+            <h1 className="text-3xl font-bold">Hello, welcome.</h1>
+            <p className="text-gray-400 mt-2">Please login your account.</p>
 
-          <div className="input flex flex-col gap-0 mt-5 px-3 py-2 h-fit items-start bg-gray-700 rounded-md text-white border-none focus-within:outline-blue_green group">
-            <p className="text-[10px] text-skyblue/85 group-focus-within:text-blue_green">Name</p>
-            <input
-              type="text"
-              name='name'
-              placeholder="Enter your name"
-              className=" w-full border-none bg-transparent placeholder-gray-400 "
-              onChange={e => onChange(e)}
-            />
-          </div>
-
-          <div className="input flex flex-col gap-0 mt-3 px-3 py-2 h-fit items-start bg-gray-700 rounded-md text-white border-none focus-within:outline-blue_green group">
-            <p className="text-[10px] text-skyblue/85 group-focus-within:text-blue_green">Password</p>
-            <div className='flex items-center justify-between w-full'>
+            <div className="input flex flex-col gap-0 mt-5 px-3 py-2 h-fit items-start bg-gray-700 rounded-md text-white border-none focus-within:outline-blue_green group">
+              <p className="text-[10px] text-skyblue/85 group-focus-within:text-blue_green">Name</p>
               <input
-                type={showPassword ? "text" : "password"}
-                name='password'
-                placeholder="Enter your password"
-                className="flex-1 border-none bg-transparent placeholder-gray-400 "
-                onChange={e => onChange(e)} />
-              <button className='text-skyblue/85 hover:text-blue_green' onClick={() => setShowPassword(!showPassword)}>
-                {
-                  !showPassword ? <IoMdEye /> : <IoMdEyeOff />
-                }
-              </button>
+                type="text"
+                name='name'
+                placeholder="Enter your name"
+                className=" w-full border-none bg-transparent placeholder-gray-400 "
+                onChange={e => onChange(e)}
+              />
             </div>
-          </div>
 
-          <a href="#" className="text-sm text-gray-400 mt-2 underline">
-            Forgot Password
-          </a>
-          <button className="mt-5 w-full min-w-[3rem] max-w-[20rem] bg-blue_green hover:bg-darkblue_green text-white font-semibold py-2 px-4 rounded-3xl"
-            onClick={e => onSubmitForm(e)}>
-            Submit
-          </button>
+            <div className="input flex flex-col gap-0 mt-3 px-3 py-2 h-fit items-start bg-gray-700 rounded-md text-white border-none focus-within:outline-blue_green group">
+              <p className="text-[10px] text-skyblue/85 group-focus-within:text-blue_green">Password</p>
+              <div className='flex items-center justify-between w-full'>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name='password'
+                  placeholder="Enter your password"
+                  className="flex-1 border-none bg-transparent placeholder-gray-400 "
+                  onChange={e => onChange(e)} />
+                <button className='text-skyblue/85 hover:text-blue_green' onClick={() => setShowPassword(!showPassword)}>
+                  {
+                    !showPassword ? <IoMdEye /> : <IoMdEyeOff />
+                  }
+                </button>
+              </div>
+            </div>
+
+            <a href="#" className="text-sm text-gray-400 mt-2 underline">
+              Forgot Password
+            </a>
+            <button className="mt-5 w-full min-w-[3rem] max-w-[20rem] bg-blue_green hover:bg-darkblue_green text-white font-semibold py-2 px-4 rounded-3xl"
+              onClick={e => onSubmitForm(e)}>
+              Submit
+            </button>
+          </div>
         </div>
 
         <div className="w-2/3 relative">
